185d8630548acff554db575ea40481717c50b2f5
Prachi Gupta <pragupta@us.ibm.com>
2020-03-12 14:05:59 -0500

    Switch conda channel to COS

--- scripts/install_wmlce.sh
+++ scripts/install_wmlce.sh
@@ -12,7 +12,8 @@ rm Miniconda3-latest-Linux-ppc64le.sh
 eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
 
 #Setup conda environment
-conda config --prepend channels https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/
+#conda config --prepend channels https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/
+conda config --prepend channels https://wml-ce-proxy-cos.s3.us-east.cloud-object-storage.appdomain.cloud/
 conda create --name wmlce_env_${WMLCE_VERSION}  python=${PYTHON_VERSION} -y
 conda activate wmlce_env_${WMLCE_VERSION}
 
